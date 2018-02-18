cbb93195420909ae275078e3cce18883da0758ea
wh75er <bokseng12@gmail.com>
2018-02-18 19:23:59 +0300

    Second listbox had been added

--- lab_00/interface.py
+++ lab_00/interface.py
@@ -31,18 +31,44 @@ class Application:
         self.canvas.create_text(690, 310, text='X')
 
         # getting ListBox object
-        self.lsbox = builder.get_object('lsbox', master)
-        self.lsbox.configure(justify=tk.CENTER)
+        self.lsbox_1 = builder.get_object('lsbox_1', master)
+        self.lsbox_1.configure(justify=tk.CENTER)
+        self.lsbox_2 = builder.get_object('lsbox_2', master)
+        self.lsbox_2.configure(justify=tk.CENTER)
 
 
+
+    def get_current_lsbox(self):
+        self.lsbox_input = tk.IntVar()
+        self.lsbox_input = self.builder.get_variable("_lsbox_now")
+        if(self.lsbox_input.get() == 1):
+            self.lsbox = self.lsbox_1
+        else:
+            self.lsbox = self.lsbox_2
 
     # debuger info 
     def debuger_write_info(self, s):
         self.var = tk.StringVar()
         self.var = self.builder.get_variable("_debug_text")
         if(self.var.get().count('\n') > 8):
-            self.var.set("| Add(x y splited by spaces)\n| remove(choice point in lsbox)\n| clean(clean all dots)")
+            self.var.set("| Add(x y splited by spaces)\n| remove(choice string in lsbox to del it)\n| clean(clean all dots)")
         self.var.set(self.var.get() + "\n> " + s)
+
+
+
+    def remove_on_click_button(self):
+        self.get_current_lsbox()
+        choice = self.lsbox.curselection()
+        if(choice != ()):
+            print(choice[0])
+        self.debuger_write_info(str(choice))
+
+
+
+    def clean_on_click_button(self):
+        self.lsbox_1.delete(first=0, last=tk.END)
+        self.lsbox_2.delete(first=0, last=tk.END)
+        self.debuger_write_info("All dots successfully deleted!")
 
 
 
@@ -50,6 +76,9 @@ class Application:
         # taking string from input
         self.inputStr = tk.StringVar()
         self.inputStr = self.builder.get_variable("_input_text")
+        # watching to witch lsbox we will add obj
+        self.get_current_lsbox()
+
         if(not self.inputStr.get()):
             return
 
