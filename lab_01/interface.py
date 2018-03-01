466792f0f387451cc0844590e02a69291c762442
wh75er <bokseng12@gmail.com>
2018-03-01 12:07:01 +0300

    few changes

--- lab_01/interface.py
+++ lab_01/interface.py
@@ -43,9 +43,11 @@ class Application:
     def draw_im(self):
         for i in range(len(self.dotLs)):
             if(i < len(self.dotLs) - 1):
-                x1, y1 = self.scale(self.dotLs[i][0], self.dotLs[i][1], 80, 80, 0, 0)
-                x2, y2 = self.scale(self.dotLs[i+1][0], self.dotLs[i+1][1], 80, 80, 0, 0)
+                x1, y1 = self.scale(self.dotLs[i][0], self.dotLs[i][1], 400, 400, 0, 0)
+                x2, y2 = self.scale(self.dotLs[i+1][0], self.dotLs[i+1][1], 400, 400, 0, 0)
                 self.canvas.create_line(300 + x1, 300 + y1*(-1), 300 + x2, 300 + y2*(-1))
+        x1, y1 = self.scale(self.dotLs[len(self.dotLs)-1][0], self.dotLs[len(self.dotLs)-1][1], 400, 400, 0, 0)
+        self.canvas.create_line(300 + x1, 300 + y1*(-1), 300 + x2, 300 + y2*(-1))
 
     def f1(self, x):
         return x**2
@@ -57,32 +59,30 @@ class Application:
     def calculate_dots(self, left_b, right_b):
         i = left_b
         node = [[], []]
-        while(i < right_b + 0.1):
-            i = round(i, 1)
+        while(i < right_b + 0.06):
             if(i > right_b):
                 break
             node[0] = i
-            node[1] = round(self.f1(i), 1)
+            node[1] = self.f1(i)
             self.dotLs.append([node[0], node[1]])
-            i += 0.1
+            i += 0.06
         i = right_b
-        while(i > -0.1):
-            i = round(i, 1)
+        while(i > -0.06):
             if(i < 0.0):
                 break
             node[0] = i
-            node[1] = round(self.f3(i), 1)
+            node[1] = self.f3(i)
             self.dotLs.append([node[0], node[1]])
-            i -= 0.1
+            i -= 0.06
         i = 0
-        while(i > left_b - 0.1):
-            i = round(i, 1)
+        while(i > left_b - 0.06):
             if(i < left_b):
                 break
             node[0] = i
-            node[1] = round(self.f2(i), 1)
+            node[1] = self.f2(i)
             self.dotLs.append([node[0], node[1]])
-            i -= 0.1
+            i -= 0.06
+        self.dotLs.append([-0.7, 0.49])
 
 
 
