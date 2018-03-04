cc7b50f1493ab7e00b8a51204331239e4ceba5fd
wh75er <bokseng12@gmail.com>
2018-03-04 15:13:05 +0300

    correct scale had been realized

--- lab_01/interface.py
+++ lab_01/interface.py
@@ -29,7 +29,15 @@ class Application:
         self.canvas = builder.get_object('canvas', master)
 
         # draw center of canvas
-        canterObj = self.canvas.create_oval(300-2, 300-2, 300+2, 300+2, width=2, fill="black", tag="center")
+        self.centerObj = self.canvas.create_oval(300-2, 300-2, 300+2, 300+2, width=2, fill="black", tag="center")
+        # tkinter variables
+            # debugger variable
+        self.var = tk.StringVar()
+        self.var = self.builder.get_variable("_debug_text")
+            # input variable
+        self.inputStr = tk.StringVar()
+        self.inputStr = self.builder.get_variable("_input_text")
+
 
         # draw main figure
         self.dotLs = []                                         # original dots
@@ -37,7 +45,10 @@ class Application:
         self.modDots = []                                       # modificated dots
         for i in range(len(self.dotLs)):
             self.modDots.append(self.dotLs[i][:])
-        self.draw_im()
+        self.init_img()
+
+        # memory steps
+        self.memoryL = []
 
 
 
@@ -54,21 +65,31 @@ class Application:
 
 
 
+    def scale_on_button_click(self):
+        if(not self.inputStr.get()):
+            return
+        
+        a = self.modDots
+        kx, ky, m1, m2 = map(float, self.inputStr.get().split())
+        for i in range(len(a)):
+            a[i][0], a[i][1] = self.scale(a[i][0], a[i][1], kx, ky, m1, m2)
+        self.draw_img()
+                
 
     def quit_on_button_click(self):
         self.master.quit()
 
 
+    def debuger_write_info(self, s):
+        if(self.var.get().count('\n') > 8):
+            self.var.set("| Welcome! waiting for commands...\n")
+        self.var.set(self.var.get() + "\n> " + s)
 
-    def draw_im(self):
-        print(self.modDots)
-        print(self.dotLs)
-        for i in range(len(self.dotLs)):
-            if(i < len(self.dotLs) - 1):
-                self.modDots[i][0], self.modDots[i][1] = self.scale(self.modDots[i][0], self.modDots[i][1], 300, 300, 0, 0)
-                x2, y2 = self.scale(self.modDots[i+1][0], self.modDots[i+1][1], 300, 300, 0, 0)
-                self.canvas.create_line(300 + self.modDots[i][0], 300 + self.modDots[i][1]*(-1), 300 + x2, 300 + y2*(-1))
-
+    def draw_img(self):
+        self.canvas.delete("img")
+        for i in range(len(self.modDots)):
+            if(i < len(self.modDots)-1):
+                self.canvas.create_line(300 + self.modDots[i][0], 300 + self.modDots[i][1]*(-1), 300 + self.modDots[i+1][0], 300 + self.modDots[i+1][1]*(-1), tag="img")
 
     def calculate_dots(self, left_b, right_b):
         i = left_b
@@ -98,6 +119,13 @@ class Application:
             i -= 0.06
         self.dotLs.append([-0.7, 0.49])
 
+    def init_img(self):
+        for i in range(len(self.modDots)):
+            if(i < len(self.modDots) - 1):
+                self.modDots[i][0], self.modDots[i][1] = self.scale(self.modDots[i][0], self.modDots[i][1], 300, 300, 0, 0)
+                x2, y2 = self.scale(self.modDots[i+1][0], self.modDots[i+1][1], 300, 300, 0, 0)
+                self.canvas.create_line(300 + self.modDots[i][0], 300 + self.modDots[i][1]*(-1), 300 + x2, 300 + y2*(-1), tag="img") 
+        self.modDots[i][0], self.modDots[i][1] = x2, y2
 
 
 
