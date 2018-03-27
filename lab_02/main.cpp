e198308322d06130d56e5ef667a490d79ada035d
wh75er <bokseng12@gmail.com>
2018-03-27 17:56:32 +0300

    Entry box working added

--- lab_02/main.cpp
+++ lab_02/main.cpp
@@ -5,18 +5,15 @@
 
 #include <gtk/gtk.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_COUNT 10
 
 
 static cairo_surface_t *surface = NULL;
 static void do_drawing(GtkWidget *widget, cairo_t *cr);
 
 
-struct {
-	int count = 0;
-	int coordx[10];
-	int coordy[10];
-	const gchar* type[10];
-}lines;
 
 
 class bg_color
@@ -35,11 +32,31 @@ class fg_color
 		gdouble b = 0 / 255.0;
 };
 
+
+struct {
+	int count = 0;
+	int coordx[MAX_LINE_COUNT];
+	int coordy[MAX_LINE_COUNT];
+	const gchar* type[MAX_LINE_COUNT];
+	fg_color color[MAX_LINE_COUNT];
+}lines;
+
+struct {
+	int start_x = 0;
+	int start_y = 0;
+	int end_x = 0;
+	int end_y = 0;
+}coordsIn;
+
 extern "C" {
 void get_alg(GtkComboBox *widget, gpointer user_data);
 void get_bg_color(GtkColorChooser *chooser, GdkRGBA *color, gpointer user_data);
 void get_fg_color(GtkColorChooser *chooser, GdkRGBA *color, gpointer user_data);
+void get_angle_str(GtkEntry *entry, gpointer user_data);
+void get_spoint_str(GtkEntry *entry, gpointer user_data);
+void get_epoint_str(GtkEntry *entry, gpointer user_data);
 
+void get_angle_on_click_button();
 void add_point();
 void quit_on_click_button();
 }
@@ -48,6 +65,13 @@ void quit_on_click_button();
 static bg_color bg_color;
 static fg_color fg_color;
 static const gchar* current_alg = "dda_alg";
+static const gchar* current_view = "auto_view";
+static int angle = 45;
+
+	static const gchar *entry_start;
+	static const gchar *entry_end;
+	static const gchar *entry_angle = "45";
+
 
 static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, 
     							gpointer user_data)
@@ -64,7 +88,7 @@ static void do_drawing(GtkWidget *widget, cairo_t *cr)
   cairo_paint(cr);
 
   cairo_set_source_rgb(cr, fg_color.r, fg_color.g, fg_color.b);
-  cairo_set_line_width(cr, 2);
+  cairo_set_line_width(cr, 1);
 
   cairo_move_to(cr, 0, 0);
   cairo_line_to(cr, 100, 100);
@@ -147,7 +171,29 @@ void get_fg_color(GtkColorChooser *chooser, GdkRGBA *color, gpointer user_data)
 	fg_color.b = cur_color.blue;
 }
 
+void get_angle_str(GtkEntry *entry, gpointer user_data)
+{
+	entry_angle = gtk_entry_get_text(entry);					// you have to press enter to comfirm it
+}
+void get_spoint_str(GtkEntry *entry, gpointer user_data)
+{
+	entry_start = gtk_entry_get_text(entry);					// you have to press enter to comfirm it
+}
+void get_epoint_str(GtkEntry *entry, gpointer user_data)
+{
+	entry_end = gtk_entry_get_text(entry);					// you have to press enter to comfirm it
+	printf("%s\n", entry_end);
+}
 
+
+
+void get_angle_on_click_button()
+{
+	if(!entry_angle)
+		return;
+
+	sscanf(entry_angle, "%d", &angle);
+}
 
 void add_point()
 {
