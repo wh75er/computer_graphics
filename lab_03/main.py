9be4426e9308dadc6dc332c3d25bd36998a6eac1
wh75er <bokseng12@gmail.com>
2018-04-26 17:32:06 +0300

    input boxes had been realized

--- lab_03/main.py
+++ lab_03/main.py
@@ -16,31 +16,63 @@ class Window(QtWidgets.QMainWindow):
         self.fg_color = QColor(Qt.black)
         self.bg_color = QColor(Qt.white)
 
-        self.bg_color_button.clicked.connect(lambda: get_bg_color(self))
-        self.fg_color_button.clicked.connect(lambda: get_fg_color(self))
+        self.center_x = 255
+        self.center_y = 255
+        self.radius = 100
+        self.amount = 30
+        self.step = 10
+        
+        self.circle = True
+
+        self.bg_color_button.clicked.connect(lambda: self.get_bg_color(self))
+        self.fg_color_button.clicked.connect(lambda: self.get_fg_color(self))
+        self.center_input.returnPressed.connect(lambda: self.get_center_coord(self))
+        self.radius_input.returnPressed.connect(lambda: self.get_radius(self))
+        self.amount_input.returnPressed.connect(lambda: self.get_amount(self))
+        self.step_input.returnPressed.connect(lambda: self.get_step(self))
         
         
 
 
-def get_bg_color(win):
-    color = QtWidgets.QColorDialog.getColor(initial = Qt.white, title = "bg color", options = QtWidgets.QColorDialog.DontUseNativeDialog)
+    def get_bg_color(self, win):
+        color = QtWidgets.QColorDialog.getColor(initial = Qt.white, title = "bg color", options = QtWidgets.QColorDialog.DontUseNativeDialog)
 
-    if color.isValid():
-        win.bg_color = color
-        win.image.fill(color)
-        s = QtWidgets.QGraphicsScene(0, 0, 10, 10);
-        s.setBackgroundBrush(color);
-        win.bg_prev.setScene(s)
-        win.scene.setBackgroundBrush(color)
+        if color.isValid():
+            win.bg_color = color
+            win.image.fill(color)
+            s = QtWidgets.QGraphicsScene(0, 0, 10, 10);
+            s.setBackgroundBrush(color);
+            win.bg_prev.setScene(s)
+            win.scene.setBackgroundBrush(color)
 
-def get_fg_color(win):
-    color = QtWidgets.QColorDialog.getColor(initial = Qt.white, title = "fg color", options = QtWidgets.QColorDialog.DontUseNativeDialog)
+    def get_fg_color(self, win):
+        color = QtWidgets.QColorDialog.getColor(initial = Qt.white, title = "fg color", options = QtWidgets.QColorDialog.DontUseNativeDialog)
 
-    if color.isValid():
-        win.fg_color = color
-        s = QtWidgets.QGraphicsScene(0, 0, 10, 10);
-        s.setBackgroundBrush(color);
-        win.fg_prev.setScene(s)
+        if color.isValid():
+            win.fg_color = color
+            s = QtWidgets.QGraphicsScene(0, 0, 10, 10);
+            s.setBackgroundBrush(color);
+            win.fg_prev.setScene(s)
+
+    def get_center_coord(self, win):
+        s = win.center_input.text();
+        self.center_x, self.center_y = s.split()
+        print(self.center_x, self.center_y)
+
+    def get_radius(self, win):
+        s = win.radius_input.text();
+        self.radius = float(s)
+        print(self.radius)
+
+    def get_amount(self, win):
+        s = win.amount_input.text();
+        self.amount = int(s)
+        print(self.amount)
+
+    def get_step(self, win):
+        s = win.step_input.text();
+        self.step = int(s)
+        print(self.step)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
